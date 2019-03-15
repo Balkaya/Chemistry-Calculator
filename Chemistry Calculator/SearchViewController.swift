9b28da7f770717032d8542dcc85a7c116ef2cc51
Can <kralciplak9@gmail.com>
2019-03-15 19:46:33 +0300

    Developed searchViewController

--- Chemistry Calculator/SearchViewController.swift
+++ Chemistry Calculator/SearchViewController.swift
@@ -12,5 +12,16 @@ class SearchViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavBar()
+        
+        tableView.backgroundColor = #colorLiteral(red: 0.1865618164, green: 0.2041032551, blue: 0.2320646985, alpha: 1)
+    }
+    
+    func setupNavBar() {
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
     }
 }
