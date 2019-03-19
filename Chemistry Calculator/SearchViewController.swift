a27473e2291c3fbc51e5cd2fe685d2ee941509cd
Can <kralciplak9@gmail.com>
2019-03-19 19:08:57 +0300

    Developed search

--- Chemistry Calculator/SearchViewController.swift
+++ Chemistry Calculator/SearchViewController.swift
@@ -9,19 +9,55 @@
 import UIKit
 
 class SearchViewController: UITableViewController {
+    var searchTexts = [String]()
+    var searching = false
 
+    let search = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupNavBar()
         
-        tableView.backgroundColor = #colorLiteral(red: 0.1865618164, green: 0.2041032551, blue: 0.2320646985, alpha: 1)
+        search.searchResultsUpdater = self as? UISearchResultsUpdating
+        
+        search.searchBar.delegate = self as? UISearchBarDelegate
+        search.obscuresBackgroundDuringPresentation = false
+        search.searchBar.placeholder = "Find a atom"
+        search.searchBar.keyboardAppearance = .dark
+        search.searchBar.tintColor = #colorLiteral(red: 0.3042124088, green: 0.8485361546, blue: 0.9998713136, alpha: 1)
+        search.searchResultsUpdater = self as? UISearchResultsUpdating
+        navigationItem.searchController = search
+        navigationItem.hidesSearchBarWhenScrolling = false
     }
     
-    func setupNavBar() {
-        navigationController?.navigationBar.prefersLargeTitles = false
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching {
+            return searchTexts.count
+        } else {
+            return atomsName.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
         
-        let searchController = UISearchController(searchResultsController: nil)
-        navigationItem.searchController = searchController
-        navigationItem.hidesSearchBarWhenScrolling = false
+        if searching {
+            cell!.textLabel?.text = searchTexts[indexPath.row]
+        } else {
+            cell!.textLabel?.text = atomsName[indexPath.row]
+        }
+        
+        return cell!
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchTexts = atomsName.filter({ $0.lowercased().prefix(searchText.count) == searchText.lowercased() })
+        searching = true
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        search.searchBar.text = ""
+        tableView.reloadData()
     }
 }
