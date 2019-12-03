2e827e8697a6cedafa91a841218b17bf99062ced
Can Balkaya <canbalkya@gmail.com>
2019-12-03 22:27:01 +0300

    Developed Settings

--- Chemistry Calculator/Settings.swift
+++ Chemistry Calculator/Settings.swift
@@ -9,20 +9,32 @@
 import SwiftUI
 
 struct Settings: View {
-    @State private var option = "All"
-    @State private var options = ["All"]
+    @State private var type = "All"
+    @State private var type2 = "Alkali metals"
+    @State private var types = ["All", "Metals", "Semimetals", "Nonmetals"]
+    @State private var types2 = ["Alkali metals", "Alkaline earths", "Halogens", "Noble gases"]
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    Picker("Option", selection: $option) {
-                        ForEach(options, id: \.self) { option in
-                            Text(option)
+                    Picker("Atom's Type", selection: $type) {
+                        ForEach(types, id: \.self) { type in
+                            Text(type)
                         }
                     }
                 }
-            }
+                Section {
+                    Picker("Atom's Type 2", selection: $type2) {
+                        ForEach(types2, id: \.self) { type2 in
+                            Text(type2)
+                        }
+                    }
+                }
+            }.navigationBarTitle(Text("Settings"), displayMode: .large)
+             .navigationBarItems(trailing: Button(action: {}) {
+                Text("Done").bold()
+            })
         }
     }
 }
